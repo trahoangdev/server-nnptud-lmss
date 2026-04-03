98a82f576ed127a75a5a6e7cb29348c0b19b8cd1
Vũ Đức Anh <vuducanh22112004@gmail.com>
2026-04-03 21:46:45 +0700

    feat: mount assignment routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,13 @@
 import express from "express";
 import authRoutes from "./auth.js";
 import classRoutes from "./classes.js";
+import assignmentRoutes from "./assignments.js";
 
 const router = express.Router();
 
 // ================== MOUNT ROUTES ==================
 router.use("/auth", authRoutes);
 router.use("/classes", classRoutes);
-
-// Các route sẽ được thêm bởi từng thành viên:
-// router.use('/assignments', assignmentRoutes);
-// ...
+router.use("/assignments", assignmentRoutes);
 
 export default router;
